9d600a374c4d53fd59643cded06075d0fc894d30
Maksym Kopychanskyi <kopy.chan.max@gmail.com>
2023-03-11 13:43:24 +0000

    search bar added

--- src/components/BeerList.jsx
+++ src/components/BeerList.jsx
@@ -7,6 +7,7 @@ import {Card, Col, Button} from "antd"
 function BeerList() {
 
     const [beersAxios, setBeersAxios] = useState([])
+    const [userInput, setUserInput] = useState("")
 
   useEffect(() => {
     axios.get("https://ih-beers-api2.herokuapp.com/beers")
@@ -15,14 +16,25 @@ function BeerList() {
 /*         console.log(response.data)
  */      })
   }, [])
+
+
+  /* Search Bar useEffect */
+  useEffect(()=>{
+    axios.get(`https://ih-beers-api2.herokuapp.com/beers/search?q=${userInput}`)
+      .then(response => {
+        setBeersAxios(response.data)
+      }) 
+  },[userInput])
   
   return (
     <div>
+      <input type="text" value={userInput} onChange={(e) => setUserInput(e.target.value)}></input>
 
         {beersAxios.map((beers) => {
           
             return(
               <Col className='column'>
+
                 <Card title={beers.name} style={{width: 230, margin: "10px 25px", }}>
                 <img src={beers.image_url} style={{width: "65px"}}/>
                 <p><em>{beers.tagline}</em></p>
